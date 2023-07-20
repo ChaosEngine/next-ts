4635c6e08160c079d0fb9832b35ad0ad7c9d3c66
ChaosEngine <andrzej.pauli@gmail.com>
2023-07-20 16:27:38 +0200

    bumps: next, @types/*, eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
         "react/react-in-jsx-scope": "off",
         // allow jsx syntax in js files (for next.js project)
         "react/jsx-filename-extension": [1, { "extensions": ['.js', '.jsx', '.ts', '.tsx'] }], //should add ".ts" if typescript project
+        "@typescript-eslint/no-explicit-any": "off"
     }
 };
